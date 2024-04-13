262fe454bd9840c3b6144a008f26511b9df8d0c4
XenZi <rilakmateja@gmail.com>
2024-04-13 10:11:31 +0200

    fix: formatting error message for displaying duplicates

--- auth-service/errors/handlers.error.go
+++ auth-service/errors/handlers.error.go
@@ -14,8 +14,8 @@ func HandleInsertError(err error, user models.Citizen) (error, int) {
 			if writeError.Code == 11000 {
 				if strings.Contains(writeError.Message, "email_1") {
 					return fmt.Errorf("Duplicate entity with email %s already exists", user.Email), 422
-				} else if strings.Contains(writeError.Message, "pid_1") {
-					return fmt.Errorf("Duplicate entity with username %s already exists", user.PersonalIdentificationNumber), 422
+				} else if strings.Contains(writeError.Message, "personalIdentificationNumber_1") {
+					return fmt.Errorf("Duplicate entity with personal identification number %s already exists", user.PersonalIdentificationNumber), 422
 				}
 			}
 		}
