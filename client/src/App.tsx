5933ef9d29ac8beb82f87ccfa2a1ab7f30209ddd
XenZi <rilakmateja@gmail.com>
2024-05-13 23:50:58 +0200

    Client update ab it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import TestPage from "./pages/Test.page";
+import NovaPage from "./pages/Nova.page";
+
 function App() {
-  return (
-    <>
-      <div className="bg-papaya-500 h-screen">
-        <h3 className="text-gunmetal-500 text-5xl">Test</h3>
-        <h3 className="text-bittersweet-500 text-5xl">Test</h3>
-        <h3 className="text-auburn-500 text-5xl">Test</h3>
-        <h3 className="text-battleship-500 text-5xl">Test</h3>
-      </div>
-    </>
-  );
+  // <>
+  //   {/* <div className="bg-papaya-500 h-screen">
+  //     <h3 className="text-gunmetal-500 text-5xl">Test</h3>
+  //     <h3 className="text-bittersweet-500 text-5xl">Test</h3>
+  //     <h3 className="text-auburn-500 text-5xl">Test</h3>
+  //     <h3 className="text-battleship-500 text-5xl">Test</h3>
+  //   </div> */}
+  // </>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      Component: TestPage,
+    },
+    {
+      path: "/test",
+      Component: NovaPage,
+    },
+  ]);
+  return <RouterProvider router={router} fallbackElement={<p>Loading</p>} />;
 }
 
 export default App;
---
+++ client/src/pages/Nova.page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NovaPage = () => {
+  return <div>NovaPage</div>;
+};
+
+export default NovaPage;
